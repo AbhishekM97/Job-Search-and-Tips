b1581845bb0bec9ae065b1097b0d38766f2c52f5
Kevin Spies <kevin.spies@gmail.com>
2019-06-29 16:10:58 -0400

    starting code to store user info in session storage

--- assets/javascript/appmain.js
+++ assets/javascript/appmain.js
@@ -1,45 +1,48 @@
 
-var firebaseConfig = {
-    apiKey: "AIzaSyBrVihpxCSnZEAR8gWi2T7kgMJXjEPTy8M",
-    authDomain: "caskjobdb.firebaseapp.com",
-    databaseURL: "https://caskjobdb.firebaseio.com",
-    projectId: "caskjobdb",
-    storageBucket: "",
-    messagingSenderId: "679758582398",
-    appId: "1:679758582398:web:39be9599762523d9"
-};
-firebase.initializeApp(firebaseConfig);
-var database = firebase.database();
-
+// var firebaseConfig = {
+//     apiKey: "AIzaSyBrVihpxCSnZEAR8gWi2T7kgMJXjEPTy8M",
+//     authDomain: "caskjobdb.firebaseapp.com",
+//     databaseURL: "https://caskjobdb.firebaseio.com",
+//     projectId: "caskjobdb",
+//     storageBucket: "",
+//     messagingSenderId: "679758582398",
+//     appId: "1:679758582398:web:39be9599762523d9"
+// };
+// firebase.initializeApp(firebaseConfig);
+// var database = firebase.database();
+var city;
+var searchstring;
 
 //grabbing the users first and last names
 $("#submit").on("click", function (event) {
+    event.preventDefault();
 
-    var firstname = $("#first_name").val().trim();
-    var lastname = $("#last_name").val().trim();
+    firstname = $("#first_name").val().trim();//"city"
+    lastname = $("#last_name").val().trim();//"search string"
+
+    sessionStorage.setItem("firstname", firstname);
+    sessionStorage.setItem("lastname", lastname);
 
 });
+city = "seattle";
+searchstring = "dog";
 
-
-firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-});
-
-
-
-//?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&tag=cats"
-// var queryURL = "https://api.indeedassessments.com/v1/";
-// var youtubeURL = "https://www.googleapis.com/youtube/v3/channels?api_key=AIzaSyAJqO0e_xPV3v6dOOSCl7U2OUNGXiSvSo0";
-//var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&tag=cats";
+//may come back to this later, but for now I will just implement session storage, and sign in with
+//first and last name
+// firebase.auth().createUserWithEmailAndPassword(firstname, lastname).catch(function (error) {
+//     // Handle Errors here.
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     // ...
+// });
 
 var queryURL = "https://en.wikipedia.org/w/api.php?action=query&prop=info&pageids=18630637&inprop=url&format=json&prop=images&gsrlimit=15&generator=search&origin=*&gsrsearch=elephant";
 var wikiURL = "http://http://en.wikipedia.org/w/api.php?action=query&format=json";
+var craigslistURL = "http://" + city + ".craigslist.org/search/sss?format=rss&query=" + searchstring;
+
 
 $.ajax({
-    url: queryURL,
+    url: craigslistURL,
     method: "GET"
 
 })
